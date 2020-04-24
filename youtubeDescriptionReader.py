d1cc0939cdf78127333a7adc6c1352a409bad11d
Zeen W <zeen-wang@hotmail.com>
2020-04-24 16:54:32 -0400

    2.2 commit

--- youtubeDescriptionReader.py
+++ youtubeDescriptionReader.py
@@ -15,25 +15,25 @@ def get_channel_videos(channel_id):
     videos = []
     res = youtube.playlistItems().list(playlistId='UUt7fwAhXDy3oNFTAzF2o8Pw', 
                                            part='snippet', 
-                                           maxResults=10,
+                                           maxResults=1,
                                            ).execute()
     videos += res['items']  
     return videos
 
 videos = get_channel_videos('theneedledrop')
 
-for video in videos:
-    reversed_score = []
-    score = ''
-    if('ALBUM REVIEW' in video['snippet']['title']):
-        title = video['snippet']['title']
-        #print(video['snippet']['description'].find('/10'))
-        index = video['snippet']['description'].find('/10')
-        while video['snippet']['description'][index] != '\n':
-            reversed_score.append(video['snippet']['description'][index])
-            index -= 1
-        for i in reversed(reversed_score):
-            score += i
-        score += '10'
-        print(title)
-        print(score)
+# for video in videos:
+#     reversed_score = []
+#     score = ''
+#     if('ALBUM REVIEW' in video['snippet']['title']):
+#         title = video['snippet']['title']
+#         #print(video['snippet']['description'].find('/10'))
+#         index = video['snippet']['description'].find('/10')
+#         while video['snippet']['description'][index] != '\n':
+#             reversed_score.append(video['snippet']['description'][index])
+#             index -= 1
+#         for i in reversed(reversed_score):
+#             score += i
+#         score += '10'
+#         print(title)
+#         print(score)
