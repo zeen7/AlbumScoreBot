55dc3b9759e5dd3dca0c70c95a688a4c17b37c4e
Zeen W <zeen-wang@hotmail.com>
2020-04-24 16:44:20 -0400

    First Commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-username=''
-password=''
-client_id=''
-client_secret=''
+username='blackafmexican'
+password='pokemonytv@Abc'
+client_id='ssi5ngVwt2nO5A'
+client_secret='Hbb6Apmkb8dTptvjrO9lQQ7OW5w'
 
 
-youtube_api_key = ''
+youtube_api_key = 'AIzaSyBR6nZ-3B1xvJvqLz8XIHbJrGHy4ymzytc'
