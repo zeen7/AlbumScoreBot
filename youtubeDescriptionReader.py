7d5e697ebda35f14d330a64a5892a6f7dfc2157d
Zeen W <zeen-wang@hotmail.com>
2020-05-02 16:15:27 -0400

    Cleaned code

--- youtubeDescriptionReader.py
+++ youtubeDescriptionReader.py
@@ -4,10 +4,6 @@ from apiclient.discovery import build
 
 youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 
-#res = youtube.channels().list(id='theneedledrop', part='contentDetails').execute()
-#res = youtube.playlistItems().list(playlistId='UUt7fwAhXDy3oNFTAzF2o8Pw', part='snippet', maxResults=5).execute()
-#print(res)
-
 def get_channel_videos(channel_id, playlist_id):
     res = youtube.channels().list(id=channel_id, 
                                   part='contentDetails').execute()
@@ -19,22 +15,3 @@ def get_channel_videos(channel_id, playlist_id):
     videos += res['items']  
     return videos
 
-# FOR DEGBUGGING
-# videos = get_channel_videos('theneedledrop', 'UUt7fwAhXDy3oNFTAzF2o8Pw')
-
-
-# for video in videos:
-#     reversed_score = []
-#     score = ''
-#     if('ALBUM REVIEW' in video['snippet']['title']):
-#         title = video['snippet']['title']
-#         #print(video['snippet']['description'].find('/10'))
-#         index = video['snippet']['description'].find('/10')
-#         while video['snippet']['description'][index] != '\n':
-#             reversed_score.append(video['snippet']['description'][index])
-#             index -= 1
-#         for i in reversed(reversed_score):
-#             score += i
-#         score += '10'
-#         print(title)
-#         print(score)
